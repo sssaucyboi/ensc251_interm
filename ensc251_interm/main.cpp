39a88844945030b361e2443515af6afb7e7a2d1a
unknown <meaninglesslegacy@gmail.com>
2021-10-11 23:06:28 -0700

    eric t.
    started working on interm project:
    
    created document of tasks required, formatting and testing
    
    deleted everything from repository initialization
    
    added files from interm project download

--- ensc251_interm/main.cpp
+++ ensc251_interm/main.cpp
@@ -1,324 +1,79 @@
-/*
-* main.cpp
-*/
-#include <stdio.h>
-#include <stdlib.h>
-#include "Date.hpp"
-#include "TimeRange.hpp"
-#include "ioUtil.hpp"
-#include "Calendar.hpp"
-#include <string>
-using namespace std;
+//main.cpp, put your driver code here, 
+//you can manipulate your class objects here
+#include <iostream> //cin and cout
+#include <fstream> //file processing
+#include <sstream> //formatted string processing
+#include <cstdlib> //atof and atoi
+#include "student.hpp"
+#include "stu_sort.hpp"
 
-// testing function
-// int main() {
-// 	Calendar testCalendar;
-// 	Date testDate(2021, 10, 7);
-// 	appointment apt1(1, 2);
+/*I provide example code here to help you read the input
+ *data from a file, so that you can focus on creating
+ *and manipulating classes and objects
+ */
+int main(){
+  //Read the domestic-stu.txt file and exit if failed
+  string line;
+  ifstream domesticFile("domestic-stu.txt");
+  if(!domesticFile.is_open()) {
+    cout << "Unable to open file domestic-stu.txt" << endl;
+    return -1;
+  }
 
-// 	std::cout << "\n\nappointment\n\n";
+  //Read the first line of domestic-stu.txt, which specifies
+  //the file format. And then print it out to the screen
+  getline(domesticFile, line);
+  cout << "File format: " << line << endl;
 
-// 	testDate.addAppointment(apt1);
-// 	testDate.printAppointedTimeSlots(std::cout);
-// 	appointment *aptPtr1 = testDate.getAppointment(0);
-// 	aptPtr1->printBookedTime(std::cout);
-// 	testDate.delAppointment(0);
+  /*Keep reading the rest of the lines in domestic-stu.txt.
+   *In the example code here, I will read each data separated
+   *by a comma, and then print it out to the screen.
+   *In your lab assignment 1, you should use these read data
+   *to initialize your DomesticStudent object. Then you can
+   *use get and set functions to manipulate your object, and
+   *print the object content to the screen
+   */
+  int stu_count = 1;
+  while( getline(domesticFile, line) ) {
+    /*process each line, get each field separated by a comma.
+     *We use istringstream to handle it.
+     *Note in this example code here, we assume the file format
+     *is perfect and do NOT handle error cases. We will leave the
+     *error and exception handling of file format to Lab Assignment 4
+     */
+    istringstream ss(line);
 
-// 	std::cout << "\n\ncalendar\n\n";
-// 	testCalendar.addDate(testDate);
-// 	Date* datePtr1 = testCalendar.getDate(testDate);
-// 	datePtr1->printFreeTimeSlots(std::cout);
-// 	testCalendar.removeDate(testDate);
+    string firstName, lastName, province, s_cgpa, s_researchScore;
+    float cgpa;
+    int researchScore;
 
-// 	std::cout << "\n\nbooking\n\n";
+    //get firstName separated by comma
+    getline(ss, firstName, ',');
 
-// 	std::cout << "\n\nend of test\n\n";
-// }
+    //get lastName separated by comma
+    getline(ss, lastName, ',');
 
-// Calendar object
-Calendar calendarObject;
+    //get province separated by comma
+    getline(ss, province, ',');
 
-// rw
-// main function
-int main();
+    //get cpga separated by comma, and convert string to float
+    getline(ss, s_cgpa, ',');
+    cgpa = atof(s_cgpa.c_str());
+    
+    //get researchScore separated by comma, and convert it to int
+    getline(ss, s_researchScore, ',');
+    researchScore = atoi(s_researchScore.c_str());
 
-// user input function for date
-void userinputD(int &enteredYear, int &enteredMonth, int &enteredDay,
-				string userenteredYear, string userenteredMonth, string userenteredDay);
+    //print the student info to the screen
+    cout << "Domestic student " << stu_count << " " << firstName << " " 
+	 << lastName << " from " << province << " province has cgpa of "
+	 << cgpa << ", and research score of " << researchScore << endl;
 
-// user input function for timerange
-void userinputTR(int &enteredbeginMinute, int &enteredbeginHour, int &enteredendMinute, int &enteredendHour,
-				 string userenteredbeginMinute, string userenteredbeginHour, string userenteredendMinute, string userenteredendHour);
-				 
-int main()
-{
-	// variables to pass into the classes (book appointment)
-	int enteredYear;
-	int enteredMonth;
-	int enteredDay;
-	int enteredbeginMinute;
-	int enteredbeginHour;
-	int enteredendMinute;
-	int enteredendHour;
+    stu_count++;
+  }
 
-	// variables to test user input (book appointment)
-	string userenteredYear;
-	string userenteredMonth;
-	string userenteredDay;
-	string userenteredbeginMinute;
-	string userenteredbeginHour;
-	string userenteredendMinute;
-	string userenteredendHour;
+  //close your file
+  domesticFile.close();
 
-	//menu variables
-	string optionMenu;
-	int optionnumberMenu;
-	string yesorno;
-
-	//// Opening Program
-	// opening statements
-	std::cout << "Welcome to the SFU Fall 2021 Calendar\n";
-
-// menu_start start point after menu error
-menu_start:
-	std::cout << "Choose an option below by typing in the corresponding number\n\n";
-	std::cout << "1) Print currently booked appointments\n";
-	std::cout << "2) Print out all days with free time slots\n";
-	std::cout << "3) Print out all free time slots for a given day\n";
-	std::cout << "4) Book an appointment\n";
-	std::cout << "5) Query an appointment\n";
-	std::cout << "6) Cancel an appointment\n";
-	std::cout << "7) Show all appointments on a date\n";
-	std::cout << "\nTo exit the program, enter 0\n";
-	std::cin >> optionMenu;
-
-	/// error checking
-	if (ioUtil::isNumerical(optionMenu) == false)
-	{
-		std::cout << "The entered option is not a number, which is not readable, try again";
-		goto menu_start;
-	}
-	// changes type string to int
-	optionnumberMenu = stoi(optionMenu);
-	// checks for non option user input
-	if ((optionnumberMenu < 0) || (optionnumberMenu > 7))
-	{
-		std::cout << "The entered option is not an option, try again";
-		goto menu_start;
-	}
-
-	// executes option chosen from menu
-	switch (optionnumberMenu)
-	{
-	// 1) Print currently booked appointments
-	case 1:
-	{
-		
-	}
-	// 2) Print out all days with free time slots
-	case 2:
-	{
-		// print function to print all days with free time slots
-		calendarObject.printFreeDays(std::cout);
-		goto menu_start;
-	}
-	// 3) Print out all free time slots for a given day
-	case 3:
-	{
-		// user input function for date
-		userinputD(enteredYear, enteredMonth, enteredDay,
-				   userenteredYear, userenteredMonth, userenteredDay);
-
-		// user entered date gets passed into the constructor function and setup date object
-		Date dateObject(enteredYear, enteredMonth, enteredDay);
-
-		// print function to print all days with free time slots
-		calendarObject.printFreeTimeSlots(std::cout, dateObject);
-		goto menu_start;
-	}
-
-	// 4) Book an appointment
-	case 4:
-	{
-	// user input function
-	option4_start:
-		// user input function for date
-		userinputD(enteredYear, enteredMonth, enteredDay,
-				   userenteredYear, userenteredMonth, userenteredDay);
-
-		// user input function for timerange
-		userinputTR(enteredbeginMinute, enteredbeginHour, enteredendMinute, enteredendHour,
-					userenteredbeginMinute, userenteredbeginHour, userenteredendMinute, userenteredendHour);
-
-		// user entered date gets passed into the constructor function and setup date object
-		Date dateObject(enteredYear, enteredMonth, enteredDay);
-
-		// user entered time range gets passed into the constructor function
-		TimeRange timerangeObject(enteredbeginMinute, enteredbeginHour, enteredendMinute, enteredendHour);
-
-		// options after the book function completes
-		if (calendarObject.book(dateObject, timerangeObject) == false)
-		{
-		option4_exit1_start:
-			cout << "\nWould you like to try another date and time? (y/n)\n";
-			cin >> yesorno;
-			if (yesorno == "y")
-			{
-				goto option4_start;
-			}
-			else if (yesorno == "n")
-			{
-				goto menu_start;
-			}
-			else
-			{
-				cout << "Entered option was unreadable, try again and type y for yes and n for no\n";
-				goto option4_exit1_start;
-			}
-		}
-		goto menu_start;
-	}
-	// 5) Query an appointment
-	case 5:
-	{
-		// user input function for date
-		userinputD(enteredYear, enteredMonth, enteredDay,
-				   userenteredYear, userenteredMonth, userenteredDay);
-
-		// user input function for timerange
-		userinputTR(enteredbeginMinute, enteredbeginHour, enteredendMinute, enteredendHour,
-					userenteredbeginMinute, userenteredbeginHour, userenteredendMinute, userenteredendHour);
-
-		// user entered date gets passed into the constructor function and setup date object
-		Date dateObject(enteredYear, enteredMonth, enteredDay);
-
-		// user entered time range gets passed into the constructor function
-		TimeRange timerangeObject(enteredbeginMinute, enteredbeginHour, enteredendMinute, enteredendHour);
-
-		// query function using calendar object
-		calendarObject.query(dateObject, timerangeObject);
-		goto menu_start;
-	}
-	// 6) Cancel an appointment
-	case 6:
-	{
-		// user input function for date
-		userinputD(enteredYear, enteredMonth, enteredDay,
-				   userenteredYear, userenteredMonth, userenteredDay);
-
-		// user input function for timerange
-		userinputTR(enteredbeginMinute, enteredbeginHour, enteredendMinute, enteredendHour,
-					userenteredbeginMinute, userenteredbeginHour, userenteredendMinute, userenteredendHour);
-
-		// user entered date gets passed into the constructor function and setup date object
-		Date dateObject(enteredYear, enteredMonth, enteredDay);
-
-		// user entered time range gets passed into the constructor function
-		TimeRange timerangeObject(enteredbeginMinute, enteredbeginHour, enteredendMinute, enteredendHour);
-
-		calendarObject.cancel(dateObject, timerangeObject);
-		goto menu_start;
-	}
-	// 7) inquire about appointment on date
-	case 7:
-	{
-		// user input function for date
-		userinputD(enteredYear, enteredMonth, enteredDay,
-				   userenteredYear, userenteredMonth, userenteredDay);
-
-		// user entered date gets passed into the constructor function and setup date object
-		Date dateObject(enteredYear, enteredMonth, enteredDay);
-		calendarObject.printAppointments(std::cout, dateObject);
-		goto menu_start;
-	}
-	// exits program
-	case 0:
-	{
-		cout << "Exiting the program\n";
-		goto menu_exit;
-	}
-	// delete this later
-	default:
-	{
-		cout << "placeholder, something went wrong";
-		break;
-	}
-	};
-
-menu_exit:
-	exit(0);
-}
-
-// user input function for date
-void userinputD(int &enteredYear, int &enteredMonth, int &enteredDay,
-				string userenteredYear, string userenteredMonth, string userenteredDay)
-{
-//// Opening Date
-// date opening statements
-userinputD_start:
-	std::cout << "Enter the date in integer format:\n"
-			  << "Example: 9/7/2021\n";
-	// user enters date in integer format
-	std::cout << "Enter Month:";
-	std::cin >> userenteredMonth;
-	std::cout << "\nEnter Day:";
-	std::cin >> userenteredDay;
-	std::cout << "\nEnter Year:";
-	std::cin >> userenteredYear;
-
-	//// User Valid Input Checks [Date]
-	// checks for non numerical input
-	if (ioUtil::isNumerical(userenteredMonth) == false || ioUtil::isNumerical(userenteredDay) == false || ioUtil::isNumerical(userenteredYear) == false)
-	{
-		std::cout << "The entered date is not a number, which is not readable, try again";
-		goto userinputD_start;
-	}
-	// changes type string to int
-	enteredYear = stoi(userenteredYear);
-	enteredMonth = stoi(userenteredMonth);
-	enteredDay = stoi(userenteredDay);
-	// checks for negative user input
-	if ((enteredYear < 0) || (enteredMonth < 0) || (enteredDay < 0))
-	{
-		std::cout << "The entered date is negative, which is not readable, try again";
-		goto userinputD_start;
-	};
-}
-// user input function for timerange
-void userinputTR(int &enteredbeginMinute, int &enteredbeginHour, int &enteredendMinute, int &enteredendHour,
-				 string userenteredbeginMinute, string userenteredbeginHour, string userenteredendMinute, string userenteredendHour)
-{
-	//// Opening Time Range
-	// time range opening statements
-userinputTR_start:
-	std::cout << "Enter the time range in multiples of 30 minutes and in 24 hour format:\n";
-	std::cout << "Example: 9:30\n";
-	// user enters time range in multiples of 30 minutes and in 24 hour format
-	std::cout << "Enter Start Hour:";
-	std::cin >> userenteredbeginHour;
-	std::cout << "\nEnter Start Minute:";
-	std::cin >> userenteredbeginMinute;
-	std::cout << "\nEnter End Hour:";
-	std::cin >> userenteredendHour;
-	std::cout << "\nEnter End Minute:";
-	std::cin >> userenteredendMinute;
-
-	//// User Valid Input Checks [TimeRange]
-	// checks for non numerical input
-	if (ioUtil::isNumerical(userenteredbeginHour) == false || ioUtil::isNumerical(userenteredbeginMinute) == false || ioUtil::isNumerical(userenteredendHour) == false || ioUtil::isNumerical(userenteredendMinute) == false)
-	{
-		std::cout << "The entered time is not a number, which is not readable, try again";
-		goto userinputTR_start;
-	}
-	// changes type string to int
-	enteredbeginHour = stoi(userenteredbeginHour);
-	enteredbeginMinute = stoi(userenteredbeginMinute);
-	enteredendHour = stoi(userenteredendHour);
-	enteredendMinute = stoi(userenteredendMinute);
-	// checks for negative user input
-	if ((enteredbeginHour < 0) || (enteredbeginMinute < 0) || (enteredendHour < 0) || (enteredendMinute < 0))
-	{
-		std::cout << "The entered time is negative, which is not readable, try again";
-		goto userinputTR_start;
-	}
+  return 0;
 }
