8a380b2f0f77d686b8935961d29a82b19d29754e
idontknowhowtocodeatall <richard_bby@hotmail.com>
2021-10-13 21:22:27 -0700

    see project log text file

--- ensc251_interm/main.cpp
+++ ensc251_interm/main.cpp
@@ -6,12 +6,88 @@
 #include <cstdlib> //atof and atoi
 #include "student.hpp"
 #include "stu_sort.hpp"
+#include "ioUtil.hpp" // made exclusively by Eric Tran
 
 /*I provide example code here to help you read the input
  *data from a file, so that you can focus on creating
  *and manipulating classes and objects
  */
+
+// functions
+int main();
+int domesticStudent();
+void exitProgram(int option1Menu, int option2Menu);
+
+// main function, prints the menu (RW)
 int main(){
+
+  // menu variables
+  string stroptionMenu;
+	int option1Menu;
+  int option2Menu;
+
+  //// main menu start
+  cout << "SFU Graduate Student Admission Program\n";
+  menu_start1:
+  std::cout << "\nSelect Domestic or International Student by typing in the corresponding number\n\n";
+	std::cout << "1) Domestic Student\n";
+	std::cout << "2) International Student\n";
+	std::cout << "\nTo exit the program, enter 0\n";
+  std::cin >> stroptionMenu;
+
+  /// error checking
+	if (ioUtil::isNumerical(stroptionMenu) == false){
+		std::cout << "The entered option is not a number, which is not readable, try again";
+		goto menu_start1;
+	}
+	// changes type string to int
+	option1Menu = stoi(stroptionMenu);
+	// checks for non option user input
+	if ((option1Menu < 0) || (option1Menu > 2)){
+		std::cout << "The entered option is not an option, try again";
+		goto menu_start1;
+	}
+  
+  // exits 
+  exitProgram(option1Menu, option2Menu);
+
+  // user selects sorting 
+  menu_start2:
+  std::cout << "\n\nSelect sort preference by typing in the corresponding number\n\n";
+	std::cout << "1) CGPA\n";
+	std::cout << "2) Research Score\n";
+  std::cout << "3) Name\n";
+	std::cout << "4) Overall Sorting (Sorts by Research Score first and then CGPA)\n";
+	std::cout << "\nTo exit the program, enter 0\n";
+  std::cin >> stroptionMenu;
+
+  /// error checking
+	if (ioUtil::isNumerical(stroptionMenu) == false){
+		std::cout << "The entered option is not a number, which is not readable, try again";
+		goto menu_start2;
+	}
+	// changes type string to int
+	option2Menu = stoi(stroptionMenu);
+	// checks for non option user input
+	if ((option2Menu < 0) || (option2Menu > 4)){
+		std::cout << "The entered option is not an option, try again";
+		goto menu_start2;
+	}
+
+  // exits 
+  exitProgram(option1Menu, option2Menu);
+
+  //// functions to print and to sort added below
+  
+};
+
+void exitProgram(int option1Menu, int option2Menu){
+  if (option1Menu == 0 || option2Menu == 0){
+    exit(0);
+  }
+};
+
+int domesticStudent(){
   //Read the domestic-stu.txt file and exit if failed
   string line;
   ifstream domesticFile("domestic-stu.txt");
